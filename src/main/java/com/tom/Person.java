0bd9d8a080c462ac72854162ff735b371a6889cf
PaulTsai20160627 <paultsai111@gmail.com>
2018-12-24 08:38:33 +0800

    Initial commit

---
+++ src/main/java/com/tom/Person.java
@@ -0,0 +1,10 @@
+package com.tom;
+
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
+public class Person {
+
+    public void hello(){
+        System.out.println("Hello World");
+    }
+}
---
+++ src/main/kotlin/com/tom/Hello.kt
@@ -0,0 +1,12 @@
+package com.tom
+
+fun main(args: Array<String>) {
+    val h = Human()
+    h.hello()
+}
+
+class Human{
+    fun hello(){
+        println("Hello World")
+    }
+}

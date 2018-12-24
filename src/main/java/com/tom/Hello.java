85214ff3a2b0154ed520021ab9a07d662b2ffa22
PaulTsai20160627 <paultsai111@gmail.com>
2018-12-24 09:10:49 +0800

    Java and Kotlin variables , data type

--- src/main/java/com/tom/Hello.java
+++ src/main/java/com/tom/Hello.java
@@ -4,5 +4,19 @@ public class Hello {
     public static void main(String[] args) {
         Person p = new Person();
         p.hello();
+
+        int age = 19;
+        Integer ageobj = 20;
+        char c = 'A';
+        Character cobj = 'B';
+        float f = 1.5F;
+        Float fobj = 1.5F;
+        double d = 1.6;
+        Double dobj = 1.6;
+        boolean b = true;
+        boolean b1 = false;
+        Boolean bobj = true;
+        byte b2 = 120;
+        Byte b2obj = 120;
     }
 }
--- src/main/kotlin/com/tom/Hello.kt
+++ src/main/kotlin/com/tom/Hello.kt
@@ -3,6 +3,10 @@ package com.tom
 fun main(args: Array<String>) {
     val h = Human()
     h.hello()
+    val age : Int = 19
+    var name : String;
+    name = "Paul"
+    var weight:Double = 55.5
 }
 
 class Human{
